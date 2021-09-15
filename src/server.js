b2db4b54e9ffa3bec6c475e7f05992aaf578a5c2
unknown <ricardogusar93@hotmail.com>
2021-09-14 20:18:21 -0500

    creating boilerplate

---
+++ src/server.js
@@ -0,0 +1,8 @@
+require("dotenv").config();
+
+const app = require("./app.js");
+const PORT = process.env.PORT || 8000;
+
+app.listen(PORT, () => {
+    console.log(`Servidor escuchando sobre el puerto ${PORT}`);
+});
